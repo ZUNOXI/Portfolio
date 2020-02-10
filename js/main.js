8e3bcbdd8c5575e626bd2f4652537796f171fa25
juno <cross9308@naver.com>
2020-02-10 19:39:34 +0900

    0210

--- js/main.js
+++ js/main.js
@@ -210,6 +210,26 @@
     });
   }
 
+/*---------------------
+  Lazy loading
+--------------------- */
+
+var bLazy = new Blazy({
+  breakpoints: [{
+    width: 420 // Max-width
+        , src: 'data-src-small'
+  }]
+  , success: function(element){
+  setTimeout(function(){
+// We want to remove the loader gif now.
+// First we find the parent container
+// then we remove the "loading" class which holds the loader image
+var parent = element.parentNode;
+parent.className = parent.className.replace(/\bloading\b/,'');
+}, 200);
+  }
+});
+
   /*---------------------
      Google Maps
   --------------------- */
