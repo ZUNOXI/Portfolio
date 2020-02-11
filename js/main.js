1ed9009ee2d321e5b3b5d70d80cd0fd13e24fee0
juno <cross9308@naver.com>
2020-02-11 10:01:22 +0900

    0211

--- js/main.js
+++ js/main.js
@@ -214,20 +214,41 @@
   Lazy loading
 --------------------- */
 
-var bLazy = new Blazy({
-  breakpoints: [{
-    width: 420 // Max-width
-        , src: 'data-src-small'
-  }]
-  , success: function(element){
-  setTimeout(function(){
-// We want to remove the loader gif now.
-// First we find the parent container
-// then we remove the "loading" class which holds the loader image
-var parent = element.parentNode;
-parent.className = parent.className.replace(/\bloading\b/,'');
-}, 200);
-  }
+document.addEventListener("DOMContentLoaded", function() {
+  let lazyImages = [].slice.call(document.querySelectorAll("img.lazy"));
+  let active = false;
+
+  const lazyLoad = function() {
+    if (active === false) {
+      active = true;
+
+      setTimeout(function() {
+        lazyImages.forEach(function(lazyImage) {
+          if ((lazyImage.getBoundingClientRect().top <= window.innerHeight && lazyImage.getBoundingClientRect().bottom >= 0) && getComputedStyle(lazyImage).display !== "none") {
+            lazyImage.src = lazyImage.dataset.src;
+            lazyImage.srcset = lazyImage.dataset.srcset;
+            lazyImage.classList.remove("lazy");
+
+            lazyImages = lazyImages.filter(function(image) {
+              return image !== lazyImage;
+            });
+
+            if (lazyImages.length === 0) {
+              document.removeEventListener("scroll", lazyLoad);
+              window.removeEventListener("resize", lazyLoad);
+              window.removeEventListener("orientationchange", lazyLoad);
+            }
+          }
+        });
+
+        active = false;
+      }, 200);
+    }
+  };
+
+  document.addEventListener("scroll", lazyLoad);
+  window.addEventListener("resize", lazyLoad);
+  window.addEventListener("orientationchange", lazyLoad);
 });
 
   /*---------------------
